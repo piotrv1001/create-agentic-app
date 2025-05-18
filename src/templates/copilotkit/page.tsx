1a047b223e77f4b156bb8ee8bf06517a98cd6dcb
piotrv1001 <piotrv1001@gmail.com>
2025-05-18 19:47:28 +0200

    feat: update copilotkit template to include gen UI example

--- src/templates/copilotkit/page.tsx
+++ src/templates/copilotkit/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { useCopilotAction } from "@copilotkit/react-core";
 import { CopilotChat } from "@copilotkit/react-ui";
 import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
+  useCopilotAction({
+    name: "weatherTool",
+    available: "disabled",
+    render: ({ status, args }) => {
+      return (
+        <p className="text-gray-500 mt-2">
+          {status !== "complete" && "Calling weather API..."}
+          {status === "complete" &&
+            `Called the weather API for ${args.location}.`}
+        </p>
+      );
+    },
+  });
+
   return (
     <div className="flex justify-center items-center h-full w-full">
       <div className="w-8/10 h-8/10 rounded-lg">
